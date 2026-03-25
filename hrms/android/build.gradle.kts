ff0c5b7747233fa48d7e8e008d8cbe845372c3bc
SUGANTHI M <suganthi0623m@gmail.com>
2026-03-25 15:49:12 +0530

    updated const

--- hrms/android/build.gradle.kts
+++ hrms/android/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.tasks.compile.JavaCompile
+
 allprojects {
     repositories {
         google()
@@ -17,6 +19,13 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    // Suppress javac warning for legacy -source/-target flags used by some plugins.
+    tasks.withType<JavaCompile>().configureEach {
+        options.compilerArgs.add("-Xlint:-options")
+    }
 }
 
 tasks.register<Delete>("clean") {
