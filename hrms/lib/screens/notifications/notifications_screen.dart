d3337e85f73ebcb99d2e2f26ffd8bd4348989d3c
mobdev2026chn <mobdev2026chn@gmail.com>
2026-08-31 18:57:46 +0530

    fix: resolve closing brackets in notifications_screen.dart

--- hrms/lib/screens/notifications/notifications_screen.dart
+++ hrms/lib/screens/notifications/notifications_screen.dart
@@ -548,6 +548,7 @@ class _NotificationsScreenState extends State<NotificationsScreen>
                         ),
                       ),
                   ],
+                ],
               ),
             ),
     );
